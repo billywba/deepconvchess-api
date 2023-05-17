5c91916495b487c119c138e7f86096d5799b5bd6
billywba <billy.wba8@gmail.com>
2023-05-17 14:42:17 +0100

    process-image: convert base64 image to tensor

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 from flask import Flask, request, jsonify
 
+import base64
+import io
+import numpy as np
+
+from PIL import Image, ImageOps
+
+from tensorflow import convert_to_tensor
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,10 +16,19 @@ def hello():
 
 @app.route('/image/process', methods=['POST'])
 def process_image():
-    image_data = request.json.get('image')
-    print(image_data)
+    base64_image = request.json.get('image')
+    image_data = base64.b64decode(base64_image)
 
-    return jsonify({'message': 'Image sent successfully.'})
+    image = Image.open(io.BytesIO(image_data))
+    image = ImageOps.exif_transpose(image)
+    image.save("test.jpg")
+
+    image_array = np.array(image)
+    tensor = convert_to_tensor(image_array)
+
+    print(tensor)
+
+    return jsonify({'message': 'image processed'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
