ceccfe7e020382a055bf09ac3f2da478e23af9d7
billywba <billy.wba8@gmail.com>
2023-05-18 17:46:21 +0100

    process-image: refactor

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 
-from utils import convert_base64_image_to_image, extract_chess_board_from_image
+from utils.utils import convert_base64_image_to_image, extract_chess_board_from_image
 
 
 app = Flask(__name__)
