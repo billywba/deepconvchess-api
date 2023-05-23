ebef8984342bc550dd23f90cfb1615b9cfdb9503
billywba <billy.wba8@gmail.com>
2023-05-23 18:42:59 +0100

    database: return percentage of games won

--- database/MoveStatisticsDatabase.py
+++ database/MoveStatisticsDatabase.py
@@ -38,11 +38,20 @@ class MoveStatisticsDatabase():
 
         results = cursor.fetchall()
         for row in results:
+            black_won = row[3]
+            white_won = row[4]
+            draw = row[5]
+            total_games = black_won + white_won + draw
+
+            black_win_percentage = (black_won / total_games) * 100
+            white_win_percentage = (white_won / total_games) * 100
+            draw_percentage = (draw / total_games) * 100
+
             move_statistics.append({
                 "move": row[2],
-                "black_won": row[3],
-                "white_won": row[4],
-                "draw": row[5]
+                "black_won": f"{black_win_percentage}%",
+                "white_won": f"{white_win_percentage}%",
+                "draw": f"{draw_percentage}%"
             })
 
         cursor.close()
