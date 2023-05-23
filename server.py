4382bfe7840a82d5aa2acde6a14d25f8c9791cb5
billywba <billy.wba8@gmail.com>
2023-05-23 22:33:06 +0100

    process-image: finished endpoints

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 
 from core.ChessBoardPositionRecogniser import ChessBoardPositionRecogniser
+from database.MoveStatisticsDatabase import MoveStatisticsDatabase
 
 from utils.utils import convert_base64_image_to_image, extract_chess_board_from_image
 
@@ -8,6 +9,8 @@ from utils.utils import convert_base64_image_to_image, extract_chess_board_from_
 app = Flask(__name__)
 
 chess_board_position_recogniser = ChessBoardPositionRecogniser()
+move_stats_db = MoveStatisticsDatabase(host='localhost', port=3306, username='root', password='root', database='move_statistics')
+
 
 @app.route('/')
 def hello():
@@ -21,9 +24,22 @@ def process_image():
     board_image = extract_chess_board_from_image(image)
 
     predicted_fen = chess_board_position_recogniser.retrieve_fen_from_image(board_image)
-    print(predicted_fen)
+    print("Predicted FEN: %s" % predicted_fen)
 
-    return jsonify({'message': 'image processed'})
+    move_statistics = move_stats_db.get_statistics(predicted_fen)
+    print(move_statistics)
+
+    return jsonify(move_statistics)
+
+@app.route('/stats', methods=['GET'])
+def get_statistics():
+    data = request.get_json()
+    fen = data.get('fen')
+
+    move_statistics = move_stats_db.get_statistics(fen)
+    print(move_statistics)
+
+    return jsonify(move_statistics)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
